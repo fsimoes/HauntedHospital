ff31c3bfd55570e9c06f0ae5eba936eb0a40cd31
Felipe Simoes <fs.simoes@gmail.com>
2017-10-28 23:42:44 -0400

    improved code, created an animation controller and removed experimental codes

---
+++ Assets/Scripts/Controllers/AnimationController.cs
@@ -0,0 +1,139 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public struct AnimationStates
+{
+    string isIdle;
+    string isDead;
+    string isGettingHit;
+    string isAttacking;
+    string isWalking;
+
+    public string IsIdle
+    {
+        get
+        {
+            return isIdle;
+        }
+
+        set
+        {
+            isIdle = value;
+        }
+    }
+
+    public string IsDead
+    {
+        get
+        {
+            return isDead;
+        }
+
+        set
+        {
+            isDead = value;
+        }
+    }
+
+    public string IsGettingHit
+    {
+        get
+        {
+            return isGettingHit;
+        }
+
+        set
+        {
+            isGettingHit = value;
+        }
+    }
+
+    public string IsAttacking
+    {
+        get
+        {
+            return isAttacking;
+        }
+
+        set
+        {
+            isAttacking = value;
+        }
+    }
+
+    public string IsWalking
+    {
+        get
+        {
+            return isWalking;
+        }
+
+        set
+        {
+            isWalking = value;
+        }
+    }
+    public AnimationStates(string isIdle = "isIdle", 
+                           string isDead = "isDead",
+                           string isGettingHit = "isGettingHit", 
+                           string isAttacking = "isAttacking", 
+                           string isWalking = "isWalking")
+    {
+        this.isIdle = isIdle;
+        this.isDead = isDead;
+        this.isGettingHit = isGettingHit;
+        this.isAttacking = isAttacking;
+        this.isWalking = isWalking;
+    }
+}
+public class AnimationController : MonoBehaviour {
+    private List<string> animationBools;
+    private Animator anim;
+    public  AnimationStates animationStates { protected set; get; }
+
+    // Use this for initialization
+    void Start () {
+        animationStates = new AnimationStates("isIdle");
+        animationBools = new List<string>();
+        string[] animStates = { animationStates.IsIdle, animationStates.IsDead, animationStates.IsGettingHit, animationStates.IsAttacking, animationStates.IsWalking };
+        animationBools.AddRange(animStates);
+        anim = GetComponentInChildren<Animator>();
+    }
+	
+    public string CurrentAnimation()
+    {
+        string currentAnimation = "";
+        foreach (string item in animationBools)
+        {
+            if (anim.GetBool(item))
+            {
+                currentAnimation = item;
+                break;
+            }
+        }
+        return currentAnimation;
+    }
+
+    public void ChangeAnimationBool(string key = "")
+    {
+        foreach (string item in animationBools)
+        {
+            anim.SetBool(item, false);
+        }
+        if (key != "")
+        {
+            anim.SetBool(key, true);
+        }
+    }
+
+    public float CurrentAnimationLength()
+    {
+        return anim.GetCurrentAnimatorStateInfo(0).length;
+    }
+
+    public bool IsThisAnimationPlaying(string animationLabel)
+    {
+        return anim.GetCurrentAnimatorStateInfo(0).IsName(animationLabel);
+    }
+}
--- Assets/Scripts/Controllers/EnemyController.cs
+++ Assets/Scripts/Controllers/EnemyController.cs
@@ -3,30 +3,36 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
 
+
 public class EnemyController : MonoBehaviour {
+
+    AnimationController animationController;
+    bool isPlayingDeathAnimation = false;
     public float lookRadius = 10f;
     public Transform target;
-    private Animator anim;
     private NavMeshAgent agent;
     int life = 100;
     bool isAlive = true;
     public float damageInterval = 5;
-    bool isPlayingDeathAnimation = false;
     float currentTime = 0;
-
     // Use this for initialization
     void Start () {
+        animationController = GetComponent<AnimationController>();
         target = target ? target:GameObject.FindGameObjectWithTag("Player").transform;
-        anim = GetComponentInChildren<Animator>();
         agent = GetComponent<NavMeshAgent>();
     }
+   
 	
 	// Update is called once per frame
 	void Update () {
         currentTime += Time.deltaTime;
         float distance = Vector3.Distance(target.position, transform.position);
-        if (anim.GetCurrentAnimatorStateInfo(0).IsName("creature1Spawn") || anim.GetCurrentAnimatorStateInfo(0).IsName("creature1roar") || !isAlive
-            || anim.GetCurrentAnimatorStateInfo(0).IsName("creature1GetHit") || anim.GetBool("isGettingHit"))
+        if (animationController.CurrentAnimation() ==  animationController.animationStates.IsIdle 
+            || animationController.CurrentAnimation() == animationController.animationStates.IsGettingHit
+            || !isAlive
+            || animationController.IsThisAnimationPlaying("Spawn")
+            || animationController.IsThisAnimationPlaying("Intro")
+            || animationController.IsThisAnimationPlaying("GetHit"))
         {
             agent.velocity = Vector3.zero;
          
@@ -39,44 +45,30 @@ public class EnemyController : MonoBehaviour {
             {
                 FaceTarget();
                 agent.velocity = Vector3.zero;
-                anim.SetBool("isIdle", false);
-                anim.SetBool("isDead", false);
-                anim.SetBool("isGettingHit", false);
-                anim.SetBool("isAttacking", true);
-                anim.SetBool("isWalking", false);
-                
+                animationController.ChangeAnimationBool(animationController.animationStates.IsAttacking);
             }
             else if(agent.speed > 0)
             {
-                anim.SetBool("isIdle", false);
-                anim.SetBool("isDead", false);
-                anim.SetBool("isAttacking", false);
-                anim.SetBool("isGettingHit", false);
-                anim.SetBool("isWalking", true);
+                animationController.ChangeAnimationBool(animationController.animationStates.IsWalking);
             }
             else
             {
-                anim.SetBool("isIdle", true);
-                anim.SetBool("isAttacking", false);
-                anim.SetBool("isDead", false);
-                anim.SetBool("isWalking", false);
-                anim.SetBool("isGettingHit", false);
-
+                animationController.ChangeAnimationBool(animationController.animationStates.IsIdle);
             }
         }
-
-        if (currentTime > damageInterval && isAlive)
+        if (animationController.IsThisAnimationPlaying("Die") && 
+            animationController.CurrentAnimation() == animationController.animationStates.IsDead && 
+            !isPlayingDeathAnimation)
         {
-            damageInterval = currentTime + 2;
-            TakeDamage(30);
-        }
-         
-        if (anim.GetCurrentAnimatorStateInfo(0).IsName("creature1Die") && !isPlayingDeathAnimation) 
-        {
-            anim.SetBool("isDead", false);
+            animationController.ChangeAnimationBool();
             isPlayingDeathAnimation = true;
-            Destroy(this.gameObject, (anim.GetCurrentAnimatorStateInfo(0).length) + 0.25f);
+            Destroy(this.gameObject, animationController.CurrentAnimationLength() + 0.25f);
         }
+        //if (currentTime > damageInterval && isAlive)
+        //{
+        //    damageInterval = currentTime + 2;
+        //    TakeDamage(20);
+        //}
     }
 
     void FaceTarget()
@@ -89,24 +81,16 @@ public class EnemyController : MonoBehaviour {
     void TakeDamage(int damage)
     {
         life -= damage;
-        anim.SetBool("isIdle", false);
-        anim.SetBool("isAttacking", false);
-        anim.SetBool("isWalking", false);
-        anim.SetBool("isDead", false);
-        anim.SetBool("isGettingHit", true);
+        animationController.ChangeAnimationBool(animationController.animationStates.IsGettingHit);
 
         if (life <= 0 && isAlive)
         {
             isAlive = false;
-            anim.SetBool("isIdle", false);
-            anim.SetBool("isAttacking", false);
-            anim.SetBool("isWalking", false);
-            anim.SetBool("isGettingHit", false);
-            anim.SetBool("isDead", true);
+            animationController.ChangeAnimationBool(animationController.animationStates.IsDead);
         }
     }
 
-  
+   
 
     private void OnDrawGizmosSelected()
     {
